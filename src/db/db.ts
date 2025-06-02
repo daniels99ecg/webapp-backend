50ad708d5405f98cf7f9dd7206de6d5b54e5ab1f
daniel <emi--lio--@hotmail.com>
2025-06-01 21:06:50 -0500

    ajustes del env

--- src/db/db.ts
+++ src/db/db.ts
@@ -9,6 +9,19 @@ const pool = new Pool({
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  max: 10,
+  idleTimeoutMillis: 10000,
+  connectionTimeoutMillis: 5000,
+});
+
+
+// Manejar errores inesperados
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  // Opcional: notificar, reiniciar app, etc.
 });
 
 export default pool;
